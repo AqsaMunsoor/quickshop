31d63d6fd3facf96a7c5b648fc2e230419f85a8b
AqsaMunsoor <aqsamansur00@gmail.com>
2025-05-14 18:40:36 +0500

    setup

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+// import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+
+import 'package:quickshop/blocs/product/cubit.dart';
+import 'package:quickshop/providers/favourite.dart';
+import 'package:quickshop/router/router.dart';
+import 'package:quickshop/router/routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        // bloc-initiate-start
+        BlocProvider(create: (_) => ProductCubit()),
+
+        ChangeNotifierProvider(create: (_) => FavoriteProvider()),
+        // bloc-initiate-end
+      ],
+      child: MaterialApp(
+        title: 'Store App',
+        debugShowCheckedModeBanner: false,
+        navigatorKey: navigator,
+        // navigatorObservers: [NavigationHistoryObserver(), routeObserver],
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: const ColorScheme.light(primary: Colors.black),
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(color: Colors.black),
+          ),
+        ),
+        routes: appRoutes,
+        initialRoute: AppRoutes.splash,
+        onGenerateRoute: onGenerateRoutes,
+      ),
+    );
+  }
+}
